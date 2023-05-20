3b292ffea5092617952cdca30800235f71b8e915
NicolasMartin02 <nico.martin000@gmail.com>
2023-05-20 23:55:51 +0700

    Login

--- GAJI/resources/views/auth/login.blade.php
+++ GAJI/resources/views/auth/login.blade.php
@@ -1,47 +1,56 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css">
+    <link href="{{ asset('/css/login.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h3 class="card-title text-center mb-4">Login</h3>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+                        <!-- Session Status -->
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <!-- Email Address -->
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+
+                            <!-- Password -->
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" />
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+
+                            <!-- Remember Me -->
+                            <div class="form-check mb-3">
+                                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                                <label for="remember_me" class="form-check-label">Remember me</label>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Log in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <!-- <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div> -->
-
-        <!-- <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif -->
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- GAJI/resources/views/sidebar.blade.php
+++ GAJI/resources/views/sidebar.blade.php
@@ -1,102 +1,136 @@
-@extends('layout.dashboard')
-@section('content')
+<?php
 
-<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-<div class="top-title">
-    <a href="/home"><img src="{{ asset('back.svg')}}" alt=""></a> <h1 style="display:inline;" class="text-white text-center"> Rekap Truck Kecil</h1>
-</div>
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KendaraanController;
 
-<div class="d-flex justify-content-center">
-    <a href="/rekap/create" class="btn btn-primary btn-lg text-center">Tambah</a>
-</div>
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Route::middleware(['auth', 'auth.session'])->group(function () {
 
-<div class="container mt-4">
-    @foreach($rekap_fuso as $key => $rekap_fusoData)
-    <div class="row">
-        <div class="col">
-            <h3>No : {{ ++$key }}</h3>
-            <p>Alamat : {{ $rekap_fusoData->alamat }}</p>
-            <p>Muat CPO : {{ $rekap_fusoData->muat_cpo }}</p>
-            <p>Tujuan Bongkar : {{ $rekap_fusoData->tujuan_bongkar }}</p>
-            <p>NO SPK : {{ $rekap_fusoData->no_spk_tanggal }}</p>
-            <p>NO Kontrak : {{ $rekap_fusoData->no_kontrak_tanggal }}</p>
-            <p>NO DO : {{ $rekap_fusoData->no_tanggal_do_besar }}</p>
-            <p>Quantity DO (ton): {{ $rekap_fusoData->quantity_do_ton }}</p>
-            <div>
-                <a href="/rekap_fuso/edit/{{ $rekap_fusoData->id }}" class="btn btn-primary">Edit</a>
-                <form action="{{ url('/rekap_fuso/delete/'.$rekap_fusoData->id) }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="hidden" name="_method" value="delete">
-                    <button type="submit" class="btn btn-danger">Hapus</button>
-                </form>
-            </div>
+    Route::get('/', function () {
+       return view('welcome');
+    });
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    Route::get('/data_tonase', [App\Http\Controllers\Data_tonase_Controller::class, 'index'])->name('data_tonase.index');
+    Route::get('/data_tonase/create', [App\Http\Controllers\Data_tonase_Controller::class, 'create'])->name('data_tonase.create');
+    Route::post('/data_tonase/store', [App\Http\Controllers\Data_tonase_Controller::class, 'store'])->name('data_tonase.store');
+    Route::get('/data_tonase/edit/{id}', [App\Http\Controllers\Data_tonase_Controller::class, 'edit'])->name('data_tonase.edit');
+    Route::patch('/data_tonase/update/{id}', [App\Http\Controllers\Data_tonase_Controller::class, 'update'])->name('data_tonase.update');
+    Route::delete('/data_tonase/delete/{id}', [App\Http\Controllers\Data_tonase_Controller::class, 'delete'])->name('data_tonase.delete');
+    
+    Route::get('/kas_uj', [App\Http\Controllers\Kas_uj_Controller::class, 'index'])->name('kas_uj.index');
+    Route::get('/kas_uj/create', [App\Http\Controllers\Kas_uj_Controller::class, 'create'])->name('kas_uj.create');
+    Route::get('/kas_uj/create_daribos', [App\Http\Controllers\Kas_uj_Controller::class, 'create_daribos'])->name('kas_uj.create_daribos');
+    Route::get('/kas_uj/create_credit', [App\Http\Controllers\Kas_uj_Controller::class, 'create_credit'])->name('kas_uj.create_credit');
+    Route::get('/kas_uj/create_debit', [App\Http\Controllers\Kas_uj_Controller::class, 'create_debit'])->name('kas_uj.create_debit');
+    Route::post('/kas_uj/store', [App\Http\Controllers\Kas_uj_Controller::class, 'store'])->name('kas_uj.store');
+    Route::post('/kas_uj/store_daribos', [App\Http\Controllers\Kas_uj_Controller::class, 'store_daribos'])->name('kas_uj.store_daribos');
+    Route::post('/kas_uj/store_credit', [App\Http\Controllers\Kas_uj_Controller::class, 'store_credit'])->name('kas_uj.store_credit');
+    Route::post('/kas_uj/store_debit', [App\Http\Controllers\Kas_uj_Controller::class, 'store_debit'])->name('kas_uj.store_debit');
+    Route::get('/kas_uj/edit/{id}', [App\Http\Controllers\Kas_uj_Controller::class, 'edit'])->name('kas_uj.edit');
+    Route::patch('/kas_uj/update/{id}', [App\Http\Controllers\Kas_uj_Controller::class, 'update'])->name('kas_uj.update');
+    Route::delete('/kas_uj/delete/{id}', [App\Http\Controllers\Kas_uj_Controller::class, 'delete'])->name('kas_uj.delete');
+    
+    Route::get('/kategori', [App\Http\Controllers\Kategori_Controller::class, 'index'])->name('kategori.index');
+    Route::get('/kategori/create', [App\Http\Controllers\Kategori_Controller::class, 'create'])->name('kategori.create');
+    Route::post('/kategori/store', [App\Http\Controllers\Kategori_Controller::class, 'store'])->name('kategori.store');
+    Route::get('/kategori/edit/{id}', [App\Http\Controllers\Kategori_Controller::class, 'edit'])->name('kategori.edit');
+    Route::patch('/kategori/update/{id}', [App\Http\Controllers\Kategori_Controller::class, 'update'])->name('kategori.update');
+    Route::delete('/kategori/delete/{id}', [App\Http\Controllers\Kategori_Controller::class, 'delete'])->name('kategori.delete');
+    
+    Route::get('/kendaraan', [App\Http\Controllers\Kendaraan_Controller::class, 'index'])->name('kendaraan.index');
+    Route::get('/kendaraan/create', [App\Http\Controllers\Kendaraan_Controller::class, 'create'])->name('kendaraan.create');
+    Route::post('/kendaraan/store', [App\Http\Controllers\Kendaraan_Controller::class, 'store'])->name('kendaraan.store');
+    Route::get('/kendaraan/edit/{id}', [App\Http\Controllers\Kendaraan_Controller::class, 'edit'])->name('kendaraan.edit');
+    Route::patch('/kendaraan/update/{id}', [App\Http\Controllers\Kendaraan_Controller::class, 'update'])->name('kendaraan.update');
+    Route::delete('/kendaraan/delete/{id}', [App\Http\Controllers\Kendaraan_Controller::class, 'delete'])->name('kendaraan.delete');
+    
+    Route::get('/muat_bongkar', [App\Http\Controllers\Muat_bongkar_Controller::class, 'index'])->name('muat_bongkar.index');
+    Route::get('/muat_bongkar/create', [App\Http\Controllers\Muat_bongkar_Controller::class, 'create'])->name('muat_bongkar.create');
+    Route::post('/muat_bongkar/store', [App\Http\Controllers\Muat_bongkar_Controller::class, 'store'])->name('muat_bongkar.store');
+    Route::get('/muat_bongkar/edit/{id}', [App\Http\Controllers\Muat_bongkar_Controller::class, 'edit'])->name('muat_bongkar.edit');
+    Route::patch('/muat_bongkar/update/{id}', [App\Http\Controllers\Muat_bongkar_Controller::class, 'update'])->name('muat_bongkar.update');
+    Route::delete('/muat_bongkar/delete/{id}', [App\Http\Controllers\Muat_bongkar_Controller::class, 'delete'])->name('muat_bongkar.delete');
+    
+    Route::get('/namasopir', [App\Http\Controllers\Namasopir_Controller::class, 'index'])->name('namasopir.index');
+    Route::get('/namasopir/create', [App\Http\Controllers\Namasopir_Controller::class, 'create'])->name('namasopir.create');
+    Route::post('/namasopir/store', [App\Http\Controllers\Namasopir_Controller::class, 'store'])->name('namasopir.store');
+    Route::get('/namasopir/edit/{id}', [App\Http\Controllers\Namasopir_Controller::class, 'edit'])->name('namasopir.edit');
+    Route::patch('/namasopir/update/{id}', [App\Http\Controllers\Namasopir_Controller::class, 'update'])->name('namasopir.update');
+    Route::delete('/namasopir/delete/{id}', [App\Http\Controllers\Namasopir_Controller::class, 'delete'])->name('namasopir.delete');
+    
+    Route::get('/rekap', [App\Http\Controllers\Rekap_Controller::class, 'index'])->name('rekap.index');
+    Route::get('/rekap/create', [App\Http\Controllers\Rekap_Controller::class, 'create'])->name('rekap.create');
+    Route::post('/rekap/store', [App\Http\Controllers\Rekap_Controller::class, 'store'])->name('rekap.store');
+    Route::get('/rekap/edit/{id}', [App\Http\Controllers\Rekap_Controller::class, 'edit'])->name('rekap.edit');
+    Route::patch('/rekap/update/{id}', [App\Http\Controllers\Rekap_Controller::class, 'update'])->name('rekap.update');
+    Route::delete('/rekap/delete/{id}', [App\Http\Controllers\Rekap_Controller::class, 'delete'])->name('rekap.delete');
+    
+    Route::get('/rekap_detail/create', [App\Http\Controllers\Rekap_Controller::class, 'createDetail'])->name('rekap_detail.create');
+    Route::post('/rekap_detail/store', [App\Http\Controllers\Rekap_Controller::class, 'storeDetail'])->name('rekap_detail.store');
+    Route::get('/rekap_detail/edit/{id}', [App\Http\Controllers\Rekap_Controller::class, 'edit'])->name('rekap_detail.edit');
+    Route::patch('/rekap_detail/update/{id}', [App\Http\Controllers\Rekap_Controller::class, 'update'])->name('rekap_detail.update');
+    Route::delete('/rekap_detail/delete/{id}', [App\Http\Controllers\Rekap_Controller::class, 'delete'])->name('rekap_detail.delete');
+    
+    Route::get('/rekap_fuso', [App\Http\Controllers\Rekap_fuso_Controller::class, 'index'])->name('rekap_fuso.index');
+    Route::get('/rekap_fuso/create', [App\Http\Controllers\Rekap_fuso_Controller::class, 'create'])->name('rekap_fuso.create');
+    Route::post('/rekap_fuso/store', [App\Http\Controllers\Rekap_fuso_Controller::class, 'store'])->name('rekap_fuso.store');
+    Route::get('/rekap_fuso/edit/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'edit'])->name('rekap_fuso.edit');
+    Route::patch('/rekap_fuso/update/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'update'])->name('rekap_fuso.update');
+    Route::delete('/rekap_fuso/delete/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'delete'])->name('rekap_fuso.delete');
+    
+    Route::get('/rekap_fusoDetail/create', [App\Http\Controllers\Rekap_fuso_Controller::class, 'createDetail'])->name('rekap_fuso_detail.create');
+    Route::post('/rekap_fusoDetail/store', [App\Http\Controllers\Rekap_fuso_Controller::class, 'storeDetail'])->name('rekap_fuso_detail.store');
+    Route::get('/rekap_fusoDetail/edit/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'edit'])->name('rekap_fuso_detail.edit');
+    Route::patch('/rekap_fusoDetail/update/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'update'])->name('rekap_fuso_detail.update');
+    Route::delete('/rekap_fusoDetail/delete/{id}', [App\Http\Controllers\Rekap_fuso_Controller::class, 'delete'])->name('rekap_fuso_detail.delete');
+    
+    Route::get('/uang_jalan', [App\Http\Controllers\Uang_jalan_Controller::class, 'index'])->name('uang_jalan.index');
+    Route::get('/uang_jalan/create', [App\Http\Controllers\Uang_jalan_Controller::class, 'create'])->name('uang_jalan.create');
+    Route::post('/uang_jalan/store', [App\Http\Controllers\Uang_jalan_Controller::class, 'store'])->name('uang_jalan.store');
+    Route::get('/uang_jalan/edit/{id}', [App\Http\Controllers\Uang_jalan_Controller::class, 'edit'])->name('uang_jalan.edit');
+    Route::patch('/uang_jalan/update/{id}', [App\Http\Controllers\Uang_jalan_Controller::class, 'update'])->name('uang_jalan.update');
+    Route::delete('/uang_jalan/delete/{id}', [App\Http\Controllers\Uang_jalan_Controller::class, 'delete'])->name('uang_jalan.delete');
+    
+    Route::get('/kendaraan/get-kategori/{id}', [App\Http\Controllers\Kendaraan_Controller::class, 'getKategori']);
+    Route::get('/muat_bongkar/get-uang_jalan/{id}', [App\Http\Controllers\Muat_bongkar_Controller::class, 'getUangjalan']);
+    Route::get('/muat_bongkar/get-tujuan/{id}', [App\Http\Controllers\Muat_bongkar_Controller::class, 'getTujuan']);
+    
+    Route::get('/update_mobil', [App\Http\Controllers\Update_mobil_Controller::class, 'index'])->name('update_mobil.index');
+    Route::get('/update_mobil/create', [App\Http\Controllers\Update_mobil_Controller::class, 'create'])->name('update_mobil.create');
+    Route::post('/update_mobil/store', [App\Http\Controllers\Update_mobil_Controller::class, 'store'])->name('update_mobil.store');
+    Route::get('/update_mobil/edit/{id}', [App\Http\Controllers\Update_mobil_Controller::class, 'edit'])->name('update_mobil.edit');
+    Route::patch('/update_mobil/update/{id}', [App\Http\Controllers\Update_mobil_Controller::class, 'update'])->name('update_mobil.update');
+    Route::delete('/update_mobil/delete/{id}', [App\Http\Controllers\Update_mobil_Controller::class, 'delete'])->name('update_mobil.delete');
 
-            <br></br>
+    Route::get('/tujuan', [App\Http\Controllers\tujuan_Controller::class, 'index'])->name('tujuan.index');
+    Route::get('/tujuan/create', [App\Http\Controllers\tujuan_Controller::class, 'create'])->name('tujuan.create');
+    Route::post('/tujuan/store', [App\Http\Controllers\tujuan_Controller::class, 'store'])->name('tujuan.store');
+    Route::get('/tujuan/edit/{id}', [App\Http\Controllers\tujuan_Controller::class, 'edit'])->name('tujuan.edit');
+    Route::patch('/tujuan/update/{id}', [App\Http\Controllers\tujuan_Controller::class, 'update'])->name('tujuan.update');
+    Route::delete('/tujuan/delete/{id}', [App\Http\Controllers\tujuan_Controller::class, 'delete'])->name('tujuan.delete');
+    
+// });
 
-            <div class="d-flex justify-content-center">
-              <a href="/rekap_fuso_detail/create" class="btn btn-primary btn-lg text-center">Tambah</a>
-            </div>
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-            <table class="table text-white table-dark table-bordered mt-4">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Tanggl Muat</th>
-                        <th scope="col">Tanggl Bongkar</th>
-                        <th scope="col">Nomor DO Kecil Transporter</th>
-                        <th scope="col">Plat Kendaraan</th>
-                        <th scope="col">Nama Sopir</th>
-                        <th scope="col">Estimasi Tonase</th>
-                        <th scope="col">Barcode PKS</th>
-                        <th scope="col" colspan="3">QTY MUAT PKS</th>
-                        <th scope="col">BRUTO</th>
-                        <th scope="col">TARRA</th>
-                        <th scope="col">NETTO</th>
-                        <th scope="col" colspan="3">QTY BONGKAR</th>
-                        <th scope="col">BRUTO</th>
-                        <th scope="col">TARRA</th>
-                        <th scope="col">NETTO</th>
-                        <th scope="col">SUSUT</th>
-                        <th scope="col">FFA/ALB</th>
-                        <th scope="col">KA</th>
-                        <th scope="col">FFA/ALB</th>
-                        <th scope="col">KA</th>
-                        <th scope="col">OVER TOLERANSI</th>
-                        <th scope="col">UANG JALAN</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($rekapDetail as $key => $rekapDetailData)
-                    <tr>
-                        <td>{{ ++$key }}</td>
-                        <td>{{ $rekapDetailData->tanggal_muat }}</td>
-                        <td>{{ $rekapDetailData->tanggal_bongkar }}</td>
-                        <td>{{ $rekapDetailData->nomor_polisi }}</td>
-                        <td>{{ $rekapDetailData->nama_sopir }}</td>
-                        <td>{{ $rekapDetailData->barcode_pks }}</td>
-                        <td>{{ $rekapDetailData->kirim_kebun }}</td>
-                        <td>{{ $rekapDetailData->terima_bulking }}</td>
-                        <td>{{ $rekapDetailData->susut_kg }}</td>
-                        <td>{{ $rekapDetailData->over_toleransi }}</td>
-                        <td>{{ $rekapDetailData->uang_jalan }}</td>
-                        <td>
-                            <a href="/muat_bongkar_detail/edit/{{ $rekapDetailData->id }}" class="btn btn-primary">Edit</a>
-                            <form action="{{ url('/muat_bongkar/delete/'.$rekapDetailData->id) }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <input type="hidden" name="_method" value="delete">
-                                <button type="submit" class="btn btn-danger">Hapus</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-    @endforeach
-</div>
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-@endsection
+require __DIR__.'/auth.php';
